1924bf03fd754b5cdf0cf2c9cb472857c8c30285
jsonsugar <djs@olexe.cn>
2014-04-23 09:29:34 +0800

    a

---
+++ XBBS.Entity/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XBBS.Models
+{
+    [PetaPoco.TableName("jexus_comments")]
+    [PetaPoco.PrimaryKey("id", autoIncrement = true)]
+    public class Comment
+    {
+        [PetaPoco.Column("id")]
+        public int ID { get; set; }
+        [PetaPoco.Column("fid")]
+        public int FId { get; set; }
+        [PetaPoco.Column("uid")]
+        public int UId { get; set; }
+        [PetaPoco.Column("content")]
+        public string Content { get; set; }
+        [PetaPoco.Column("replytime")]
+        public DateTime Replytime { get; set; }
+    }
+}
---
+++ XBBS.Entity/Targs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XBBS.Models
+{
+    [PetaPoco.TableName("jexus_tags")]
+    [PetaPoco.PrimaryKey("tag_id", autoIncrement = true)]
+    public class Targs
+    {
+        [PetaPoco.Column("tag_id")]
+        public int TagID { get; set; }
+
+        [PetaPoco.Column("tag_title")]
+        public string Title { get; set; }
+
+        [PetaPoco.Column("forums")]
+        public int Forums { get; set; }
+
+        [PetaPoco.Ignore()]
+        public int Total { get; set; }
+    }
+
+
+    [PetaPoco.TableName("stb_tags_relation")]
+    public class TargsRelation
+    {
+        [PetaPoco.Column("tag_id")]
+        public int TagID { get; set; }
+
+        [PetaPoco.Column("fid")]
+        public int ForumsId { get; set; }
+    }
+}
